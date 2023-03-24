da5fb6b39f7defdac80fb32a6dda1cae2c6e137d
joseangelayalaz <ayalazjangel@gmail.com>
2023-03-24 11:02:01 -0600

    Made my first hello world in java and saw some examples

---
+++ src/holaMundo.java
@@ -0,0 +1,5 @@
+public class holaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola Nuevo Mundo De Angel, Triunfarás en esto, pronto conseguirás ese gran empleo");
+    }
+}
---
+++ src/variables.java
@@ -0,0 +1,21 @@
+public class variables {
+    public static void main(String[] args) {
+        int salary = 1000;
+        salary = salary + 200;
+        System.out.println(salary); // 1200
+
+        salary += 300;
+        System.out.println(salary); // 1500
+
+        // Actualizar variables de tipo String:
+        String employeeName = " Anahí";
+        employeeName = employeeName + " Salgado";
+        System.out.println(salary); // Anahí Salgado
+
+        employeeName += " Díaz de la Vega";
+        System.out.println(employeeName); // Anahí Salgado Díaz de la Vega
+
+        employeeName = "Irene" + employeeName;
+        System.out.println(employeeName);
+    }
+}
